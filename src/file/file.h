75bef0e62ae0fbf0b47ea8d50691aa4fed430b66
Caio Gabriel <caiogtoj@gmail.com>
2023-10-20 09:12:52 -0300

    Atualizações do WordFinder

---
+++ src/file/file.h
@@ -0,0 +1,11 @@
+//
+// Created by caiog on 20/10/2023.
+//
+
+#ifndef WORDFINDER_FILE_H
+#define WORDFINDER_FILE_H
+
+/** Cabeçalho para inserir as funções de leitura e validação das palavras
+    do documento em .txt que serão lidas. **/
+
+#endif //WORDFINDER_FILE_H
